39c2dd5f7cf706cfc3de75da714c360f3487e3d9
Thom Treebus <73241617+thomtreebus@users.noreply.github.com>
2021-08-31 22:09:36 +0200

    Add files via upload

---
+++ astar.py
@@ -0,0 +1,279 @@
+import pygame
+import math
+from queue import PriorityQueue
+import heapq as hq
+
+pygame.init()
+
+#Window Setup
+WIDTH = 800
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH+50))
+
+pygame.display.set_caption("A* Pathfinding Visualizer")
+
+# GUI Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE= (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+# Instruction label at the bottom of the screen
+WINDOW.fill(WHITE)
+font = pygame.font.SysFont('Arial', 24)
+text = font.render(' LMOUSE = start/end point + barrier || RMOUSE = erase || C = clear || SPACE = start', True, BLACK, WHITE)
+text_rect = text.get_rect()
+text_rect.center = (WIDTH // 2, 825)
+
+pygame.draw.line(WINDOW, RED, (0, 800), (800, 800), 5)
+
+class Point:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.width = width 
+        self.total_rows = total_rows
+
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == PURPLE
+
+    #
+    # Methods to change the color of a point
+    #
+
+    def reset(self):
+        self.color = WHITE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+    
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_start(self):
+        self.color = ORANGE
+    
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        self.neighbors = []
+        # Down
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+        # Up
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+        # Right
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
+        # Left
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col - 1])
+
+    def __lt__(self, other):
+        return False
+
+# Calculate heuristic
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1-y2)
+
+# A* pathfinding algorithm
+def a_star(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {point: float("inf") for row in grid for point in row}
+    g_score[start] = 0
+    f_score = {point: float("inf") for row in grid for point in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            reconstruct_path(came_from, start, end, draw)
+            end.make_end()
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
+
+# Reconstruct the path that the algorithm discovered
+def reconstruct_path(came_from, start, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+    start.make_start()
+
+
+# Create a grid of points
+def make_grid(rows, width):
+    grid =[]
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            point = Point(i, j, gap, rows)
+            grid[i].append(point)
+
+    return grid
+
+# Draw the grid
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+# Draw all of the points
+def draw(win, grid, rows, width):
+	for row in grid:
+		for point in row:
+			point.draw(win)
+
+	draw_grid(win, rows, width)
+	pygame.display.update()
+
+# Get the grid position of where the mouse was clicked
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+    
+    row = y // gap
+    col = x // gap
+
+    return row, col 
+
+# Main method
+def main(win, width):
+    rows = 50
+    grid = make_grid(rows, width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+
+    win.blit(text, text_rect)
+
+    while run:
+        draw(win, grid, rows, width)
+
+        # Event handlers
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            # Left mouse button
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                
+                if pos[1] < 800:
+                    row, col = get_clicked_pos(pos, rows, width)
+                    point = grid[row][col] 
+                    if not start and point != end:
+                        start = point
+                        start.make_start()
+                    
+                    elif not end and point != start:
+                        end = point
+                        end.make_end()
+                    
+                    elif point != end and point != start:
+                        point.make_barrier()
+
+            # Right mouse button
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, rows, width)
+                point = grid[row][col]
+                point.reset()
+
+                if point == start:
+                    start = None
+                elif point == end:
+                    end = None
+            
+            # Event handlers for different key presses
+            if event.type == pygame.KEYDOWN:
+                # Start the pathfinding
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for point in row:
+                            point.update_neighbors(grid)
+                    a_star(lambda: draw(win, grid, rows, width), grid, start, end)
+
+                # Cler the grid
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(rows, width)
+    pygame.quit()
+
+
+main(WINDOW, WIDTH)
